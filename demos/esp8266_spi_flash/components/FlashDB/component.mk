00ddb6b430e5aaeefba5c8c04cf39a62e8345ade
suyoujiang1 <suyoujiang1@leedarson.com>
2020-10-19 13:47:00 +0800

    [demo] add esp8266 demo.

---
+++ demos/esp8266_spi_flash/components/FlashDB/component.mk
@@ -0,0 +1,19 @@
+#
+# Component Makefile
+#
+COMPONENT_ADD_INCLUDEDIRS += \
+	inc \
+	../../../../inc \
+	../../../../port/fal/inc
+
+COMPONENT_SRCDIRS += \
+	../../../../src \
+	../../../../samples \
+	../../../../port/fal/src \
+	porting
+
+
+# COMPONENT_PRIV_INCLUDEDIRS +=
+
+# COMPONENT_ADD_LDFLAGS += -Wl,--whole-archive -l$(COMPONENT_NAME) -Wl,--no-whole-archive
+
