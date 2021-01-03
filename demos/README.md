60cee13a1d1d227ac37dad5b4e12afbc1beca231
armink <armink.ztl@gmail.com>
2021-01-03 22:37:16 +0800

    Add linux platform demo.

--- demos/README.md
+++ demos/README.md
@@ -1,6 +1,8 @@
-| folder name           | flash type    |
-| :-------------------- | :------------ |
-| stm32f103ve           | stm32 on-chip |
-| stm32f405rg           | stm32 on-chip |
-| stm32f405rg_spi_flash | spi flash     |
+| folder name           | storage type        |
+| :-------------------- | :------------------ |
+| stm32f103ve           | stm32 on-chip flash |
+| stm32f405rg           | stm32 on-chip flash |
+| stm32f405rg_spi_flash | spi flash           |
+| esp8266_spi_flash     | spi flash           |
+| linux                 | posix file          |
 
---
+++ demos/linux/applications/fdb_cfg.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020, Armink, <armink.ztl@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief configuration file
+ */
+
+#ifndef _FDB_CFG_H_
+#define _FDB_CFG_H_
+
+/* using KVDB feature */
+#define FDB_USING_KVDB
+
+#ifdef FDB_USING_KVDB
+/* Auto update KV to latest default when current KVDB version number is changed. @see fdb_kvdb.ver_num */
+/* #define FDB_KV_AUTO_UPDATE */
+#endif
+
+/* using TSDB (Time series database) feature */
+#define FDB_USING_TSDB
+
+/* Using file storage mode by POSIX file API, like open/read/write/close */
+#define FDB_USING_FILE_POSIX_MODE
+
+/* log print macro. default EF_PRINT macro is printf() */
+/* #define FDB_PRINT(...)              my_printf(__VA_ARGS__) */
+
+/* print debug information */
+#define FDB_DEBUG_ENABLE
+
+#endif /* _FDB_CFG_H_ */
