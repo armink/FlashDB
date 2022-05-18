03500fa92568a0fba944ed44877fb72ec74fd90c
朱天龙 (Armink) <armink.ztl@gmail.com>
2022-05-18 20:27:46 +0800

    update the version number.

--- inc/fdb_def.h
+++ inc/fdb_def.h
@@ -17,8 +17,8 @@ extern "C" {
 #endif
 
 /* software version number */
-#define FDB_SW_VERSION                 "1.1.2"
-#define FDB_SW_VERSION_NUM             0x10102
+#define FDB_SW_VERSION                 "1.2.0"
+#define FDB_SW_VERSION_NUM             0x10200
 
 /* the KV max name length must less then it */
 #ifndef FDB_KV_NAME_MAX
