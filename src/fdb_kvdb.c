a438284d5a6f361ca884fbcc82109c67674f7166
Lyon <88232613+pikasTech@users.noreply.github.com>
2023-10-15 22:48:30 +0800

    Update src/fdb_kvdb.c
    Co-authored-by: 朱天龙 (Armink) <armink.ztl@gmail.com>

--- src/fdb_kvdb.c
+++ src/fdb_kvdb.c
@@ -841,7 +841,8 @@ static void sector_iterator(fdb_kvdb_t db, kv_sec_info_t sector, fdb_sector_stor
             return;
         if (status == FDB_SECTOR_STORE_UNUSED || status == sector->status.store) {
             if (traversal_kv) {
-                if (FDB_NO_ERR != read_sector_info(db, sec_addr, sector, true))return;
+                if (FDB_NO_ERR != read_sector_info(db, sec_addr, sector, true))
+                    return;
             }
             /* iterator is interrupted when callback return true */
             if (callback && callback(sector, arg1, arg2)) {
