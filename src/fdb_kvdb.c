3206f58c99134c32f9a655498ea8e1b30c735cd6
Mr.Chip <mrchip.zxf@outlook.com>
2024-05-10 14:44:33 +0800

    [fdb_kvdb] fix lock issue to fix block on first run

--- src/fdb_kvdb.c
+++ src/fdb_kvdb.c
@@ -1798,9 +1798,11 @@ fdb_err_t fdb_kvdb_init(fdb_kvdb_t db, const char *name, const char *path, struc
 #endif /* FDB_KV_USING_CACHE */
 
     FDB_DEBUG("KVDB size is %" PRIu32 " bytes.\n", db_max_size(db));
-
+    db_unlock(db);
+    
     result = _fdb_kv_load(db);
-
+    
+    db_lock(db);
 #ifdef FDB_KV_AUTO_UPDATE
     if (result == FDB_NO_ERR) {
         kv_auto_update(db);
