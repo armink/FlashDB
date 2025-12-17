armink/FlashDB

commit 0d3899a94436047ae31a20322aaa077ae7a6cdd2

Improve FLASH_WRITE reusability as requested in PR review

src/fdb_tsdb.c
@@ -320,12 +320,7 @@ static fdb_err_t write_tsl(fdb_tsdb_t db, fdb_blob_t blob, fdb_time_t time)
     fdb_err_t result = FDB_NO_ERR;
     struct log_idx_data idx;
     uint32_t idx_addr = db->cur_sec.empty_idx;
-#ifdef FDB_TSDB_FIXED_BLOB_SIZE
-     uint32_t log_addr = db->cur_sec.empty_data - FDB_WG_ALIGN(FDB_TSDB_FIXED_BLOB_SIZE);
-#else
-    idx.log_len = blob->size;
-    idx.log_addr = db->cur_sec.empty_data - FDB_WG_ALIGN(idx.log_len);
-#endif
+    uint32_t log_addr = db->cur_sec.empty_data - FDB_WG_ALIGN(blob->size);
 
     idx.time = time;
 
@@ -334,11 +329,7 @@ static fdb_err_t write_tsl(fdb_tsdb_t db, fdb_blob_t blob, fdb_time_t time)
     /* write other index info */
     FLASH_WRITE(db, idx_addr + LOG_IDX_TS_OFFSET, &idx.time,  sizeof(struct log_idx_data) - LOG_IDX_TS_OFFSET, false);
     /* write blob data */
-#ifdef FDB_TSDB_FIXED_BLOB_SIZE
     FLASH_WRITE(db, log_addr, blob->buf, blob->size, false);
-#else
-    FLASH_WRITE(db, idx.log_addr, blob->buf, blob->size, false);
-#endif
     /* write the status will by write granularity */
     _FDB_WRITE_STATUS(db, idx_addr, idx.status_table, FDB_TSL_STATUS_NUM, FDB_TSL_WRITE, true);
 

commit 6dcd4adaa11456be04bee83f7561032f1730c093

tsdb: fix missing log address/size for variable blobs
This regression was introduced by previous commit 0d3899a.

src/fdb_tsdb.c
@@ -322,6 +322,11 @@ static fdb_err_t write_tsl(fdb_tsdb_t db, fdb_blob_t blob, fdb_time_t time)
     uint32_t idx_addr = db->cur_sec.empty_idx;
     uint32_t log_addr = db->cur_sec.empty_data - FDB_WG_ALIGN(blob->size);
 
+#ifndef FDB_TSDB_FIXED_BLOB_SIZE
+    // variable-size blobs must store address and size in flash
+    idx.log_addr = log_addr;
+    idx.log_len = blob->size;
+#endif
     idx.time = time;
 
     /* write the status will by write granularity */