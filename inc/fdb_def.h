60f65550bce7fda525cf131edeeb35c692baef2b
朱天龙 (Armink) <armink.ztl@gmail.com>
2025-12-23 08:42:26 +0800

    feat: Update software version to 2.1.99

--- inc/fdb_def.h
+++ inc/fdb_def.h
@@ -21,8 +21,8 @@ extern "C" {
 #endif
 
 /* software version number */
-#define FDB_SW_VERSION                 "2.1.1"
-#define FDB_SW_VERSION_NUM             0x20101
+#define FDB_SW_VERSION                 "2.1.99"
+#define FDB_SW_VERSION_NUM             0x20199
 
 /* the KV max name length must less then it */
 #ifndef FDB_KV_NAME_MAX
