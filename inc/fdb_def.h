65a87640bf816effc84ca161f5254878677232b8
朱天龙 (Armink) <armink.ztl@gmail.com>
2026-03-23 18:59:33 +0800

    feat: update the version number to v2.2.0

--- inc/fdb_def.h
+++ inc/fdb_def.h
@@ -21,8 +21,8 @@ extern "C" {
 #endif
 
 /* software version number */
-#define FDB_SW_VERSION                 "2.1.99"
-#define FDB_SW_VERSION_NUM             0x20199
+#define FDB_SW_VERSION                 "2.2.0"
+#define FDB_SW_VERSION_NUM             0x20200
 
 /* the KV max name length must less then it */
 #ifndef FDB_KV_NAME_MAX
